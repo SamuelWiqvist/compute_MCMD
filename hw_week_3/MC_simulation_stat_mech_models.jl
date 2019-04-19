96118902d57f35ddb98d05aa53b7741cae6d054d
Samuel <swiqvist@gmail.com>
2019-04-19 13:01:06 +0200

    MC simulations for ising model done

--- hw_week_3/MC_simulation_stat_mech_models.jl
+++ hw_week_3/MC_simulation_stat_mech_models.jl
@@ -2,28 +2,19 @@ using PyPlot
 
 # Warmup simulte the Ising model using the Metroplis algorighm
 
-
-dim = 500
-
 # generate start condiguration for stat S
+dim = 100
 S_start = zeros(dim,dim)
 map!(x -> x = rand([1,-1]), S_start, S_start)
 
 PyPlot.figure()
 PyPlot.imshow(S_start,cmap="hot", interpolation="nearest")
-PyPlot.colorbar()
-
-# set system parameters
-J = 1 # interaction strength (we have the same interaction strength for all states)
-β = 10 # tempering
 
 # the energy function for the ising model
 function H(S, J)
 
     energy = 0
 
-
-    energy = 0
     for i = 1:size(S,1) # column
         for j = 1:size(S,2) # row
 
@@ -100,9 +91,94 @@ function H(S, J)
 
 end
 
+# metroplis algorithm to simulate the ising model
+function metroplis(S_start, iter, J, β)
 
-S_start
+    nbr_stats = length(S_start)
+
+    S_configs = zeros(iter, size(S_start,1),size(S_start,2))
+    a_vec = zeros(iter)
+    energy_vec = zeros(iter)
+    S_configs[1,:,:] = S_start
+
+    H_old = H(S_configs[1,:,:], J)
+    energy_vec[1] = H_old
+
+    α_log = log(1)
+
+    for i = 2:iter
+
+        # select state to flip at random
+        S_update = S_configs[i-1,:,:]
+        S_flip = rand(1:nbr_stats)
+        S_update[S_flip] = -1*S_update[S_flip]
+
+        H_new = H(S_update,J)
+
+        if H_new > H_old
+            α_log = -β*(H_new-H_old)
+        else
+            α_log = log(1)
+        end
+
+        if log(rand()) < α_log # accapt new config
+            S_configs[i,:,:] = S_update
+            H_old = H_new
+            a_vec[i] = 1
+            energy_vec[i] = H_new
+        else
+            S_configs[i,:,:] = S_configs[i-1,:,:]
+            energy_vec[i] = H_old
+
+        end
+
+    end
+
+    return S_configs,energy_vec,a_vec
+
+end
 
 
-# test energy function
-@time H(S_start, J)
+
+# set system parameters
+J = 0.5 # interaction strength (we have the same interaction strength for all states)
+β = 20 # tempering
+iter = 200000 # nbr of MC interstions
+
+
+S_configs,energy_vec, a_vec = @time metroplis(S_start, iter, J, β)
+
+
+sum(a_vec)/iter
+
+PyPlot.figure()
+PyPlot.plot(1:iter, energy_vec)
+
+iter_plot = floor.(Int,LinRange(1,iter,100))
+
+PyPlot.figure()
+for i = iter_plot
+    sleep(0.001)
+    PyPlot.imshow(S_configs[i,:,:],cmap="hot", interpolation="nearest")
+    PyPlot.xlabel(i)
+end
+
+
+
+PyPlot.figure()
+PyPlot.imshow(S_configs[1,:,:],cmap="hot", interpolation="nearest")
+
+PyPlot.figure()
+PyPlot.imshow(S_configs[1000,:,:],cmap="hot", interpolation="nearest")
+
+PyPlot.figure()
+PyPlot.imshow(S_configs[10000,:,:],cmap="hot", interpolation="nearest")
+
+PyPlot.figure()
+PyPlot.imshow(S_configs[100000,:,:],cmap="hot", interpolation="nearest")
+
+PyPlot.figure()
+PyPlot.imshow(S_configs[150000,:,:],cmap="hot", interpolation="nearest")
+
+PyPlot.figure()
+PyPlot.imshow(S_configs[200000,:,:],cmap="hot", interpolation="nearest")
