41cda8b2602d69f05afc5c51b892b444b33aa794
Samuel <swiqvist@gmail.com>
2019-04-21 18:20:28 +0200

    stuff for potts model with genreal d

--- hw_week_3/MC_simulation_stat_mech_models.jl
+++ hw_week_3/MC_simulation_stat_mech_models.jl
@@ -1,14 +1,16 @@
 using PyPlot
 
-# Warmup simulte the Ising model using the Metroplis algorighm
+################################################################################
+# Warmup: simulate the Ising model using the Metroplis algorithm
+################################################################################
 
 # generate start condiguration for stat S
-dim = 100
-S_start = zeros(dim,dim)
+dims = 100
+S_start = zeros(dims,dims)
 map!(x -> x = rand([1,-1]), S_start, S_start)
 
 PyPlot.figure()
-PyPlot.imshow(S_start,cmap="hot", interpolation="nearest")
+PyPlot.imshow(S_start, cmap="hot", interpolation="nearest")
 
 # the energy function for the ising model
 function H(S, J)
@@ -27,7 +29,6 @@ function H(S, J)
             end
 
             # nearest neighbors for corner in first column
-
             if i == 1 && j == 1
                 energy= energy + S[i,j]*S[i,j+1]
                 energy= energy + S[i,j]*S[i+1,j]
@@ -49,16 +50,13 @@ function H(S, J)
             end
 
             # nearest neighbors for states in first column
-
             if i == 1 && j > 1 && j <= size(S,2)-1
                 energy= energy + S[i,j]*S[i,j+1]
                 energy= energy + S[i,j]*S[i+1,j]
                 energy= energy + S[i,j]*S[i,j-1]
             end
 
-
             # nearest neighbors for states in last column
-
             if i == size(S,1) && j > 1 && j <= size(S,2)-1
                 energy= energy + S[i,j]*S[i,j+1]
                 energy= energy + S[i,j]*S[i-1,j]
@@ -66,7 +64,6 @@ function H(S, J)
             end
 
             # nearest neighbors for states in first row
-
             if j == 1 && i > 1 && i <= size(S,1)-1
                 energy= energy + S[i,j]*S[i,j+1]
                 energy= energy + S[i,j]*S[i-1,j]
@@ -74,18 +71,13 @@ function H(S, J)
             end
 
             # nearest neighbors for states in first last
-
             if j == size(S,2) && i > 1 && i <= size(S,1)-1
                 energy= energy + S[i,j]*S[i,j-1]
                 energy= energy + S[i,j]*S[i-1,j]
                 energy= energy + S[i,j]*S[i+1,j]
             end
-
-
          end
-
     end
-
 
     return -J*energy
 
@@ -94,27 +86,25 @@ end
 # metroplis algorithm to simulate the ising model
 function metroplis(S_start, iter, J, β)
 
-    nbr_stats = length(S_start)
-
+    nbr_stats = length(S_start) # set up
     S_configs = zeros(iter, size(S_start,1),size(S_start,2))
     a_vec = zeros(iter)
     energy_vec = zeros(iter)
     S_configs[1,:,:] = S_start
 
-    H_old = H(S_configs[1,:,:], J)
+    H_old = H(S_configs[1,:,:], J) # first iteration
     energy_vec[1] = H_old
-
     α_log = log(1)
 
     for i = 2:iter
 
-        # select state to flip at random
-        S_update = S_configs[i-1,:,:]
-        S_flip = rand(1:nbr_stats)
-        S_update[S_flip] = -1*S_update[S_flip]
+        S_update = S_configs[i-1,:,:] # select state to flip at random
+        S_flip = rand(1:nbr_stats) # flip state
+        S_update[S_flip] = -1*S_update[S_flip] # set prop config
 
-        H_new = H(S_update,J)
+        H_new = H(S_update,J) # compute energy for prop config
 
+        # compute acc prob
         if H_new > H_old
             α_log = -β*(H_new-H_old)
         else
@@ -127,7 +117,7 @@ function metroplis(S_start, iter, J, β)
             a_vec[i] = 1
             energy_vec[i] = H_new
         else
-            S_configs[i,:,:] = S_configs[i-1,:,:]
+            S_configs[i,:,:] = S_configs[i-1,:,:] # store old config
             energy_vec[i] = H_old
 
         end
@@ -138,19 +128,18 @@ function metroplis(S_start, iter, J, β)
 
 end
 
-
-
 # set system parameters
 J = 0.5 # interaction strength (we have the same interaction strength for all states)
 β = 20 # tempering
 iter = 200000 # nbr of MC interstions
 
+# run Metroplis algorithm
+S_configs, energy_vec, a_vec = @time metroplis(S_start, iter, J, β)
 
-S_configs,energy_vec, a_vec = @time metroplis(S_start, iter, J, β)
-
-
+# compute avg acc prob
 sum(a_vec)/iter
 
+# plotting
 PyPlot.figure()
 PyPlot.plot(1:iter, energy_vec)
 
@@ -159,31 +148,31 @@ iter_plot = floor.(Int,LinRange(1,iter,100))
 PyPlot.figure()
 for i = iter_plot
     sleep(0.001)
-    PyPlot.imshow(S_configs[i,:,:],cmap="hot", interpolation="nearest")
+    PyPlot.imshow(S_configs[i,:,:], cmap="hot", interpolation="nearest")
     PyPlot.xlabel(i)
 end
 
-
+PyPlot.figure()
+PyPlot.imshow(S_configs[1,:,:], cmap="hot", interpolation="nearest")
 
 PyPlot.figure()
-PyPlot.imshow(S_configs[1,:,:],cmap="hot", interpolation="nearest")
+PyPlot.imshow(S_configs[1000,:,:], cmap="hot", interpolation="nearest")
 
 PyPlot.figure()
-PyPlot.imshow(S_configs[1000,:,:],cmap="hot", interpolation="nearest")
+PyPlot.imshow(S_configs[10000,:,:], cmap="hot", interpolation="nearest")
 
 PyPlot.figure()
-PyPlot.imshow(S_configs[10000,:,:],cmap="hot", interpolation="nearest")
+PyPlot.imshow(S_configs[100000,:,:], cmap="hot", interpolation="nearest")
 
 PyPlot.figure()
-PyPlot.imshow(S_configs[100000,:,:],cmap="hot", interpolation="nearest")
+PyPlot.imshow(S_configs[150000,:,:], cmap="hot", interpolation="nearest")
 
 PyPlot.figure()
-PyPlot.imshow(S_configs[150000,:,:],cmap="hot", interpolation="nearest")
+PyPlot.imshow(S_configs[200000,:,:], cmap="hot", interpolation="nearest")
 
-PyPlot.figure()
-PyPlot.imshow(S_configs[200000,:,:],cmap="hot", interpolation="nearest")
-
-# Potts model
+################################################################################
+# Potts model 2D q = 10
+################################################################################
 
 dim = 2
 dims = 100
@@ -220,7 +209,7 @@ end
 
 t = Tuple([idx[1],idx[2]] + moves[1,:])
 
-LinearIndices(S_start)[t[:]]
+LinearIndices(S_start)
 
 S_start[100]
 
