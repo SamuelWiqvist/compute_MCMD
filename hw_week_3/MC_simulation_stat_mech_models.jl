b22d01138eb4e83859f6a9dfb61981237332ac36
Samuel <swiqvist@gmail.com>
2019-04-23 15:51:28 +0200

    wang-landau w iteration

--- hw_week_3/MC_simulation_stat_mech_models.jl
+++ hw_week_3/MC_simulation_stat_mech_models.jl
@@ -2,6 +2,7 @@
 
 using PyPlot
 using Printf
+using LaTeXStrings
 
 
 ################################################################################
@@ -158,7 +159,7 @@ PyPlot.figure(figsize=(8,5))
 PyPlot.plot(1:iter, energy_vec)
 PyPlot.xlabel("Iteration")
 PyPlot.ylabel("Energy")
-PyPlot.savefig("hw_week_3/fig/energy_vs_iter.eps", format="eps", dpi=1000)
+PyPlot.savefig("hw_week_3/fig/ising_energy_vs_iter.eps", format="eps", dpi=1000)
 
 iter_plot = floor.(Int,LinRange(1,iter,100))
 
@@ -194,22 +195,11 @@ PyPlot.xlabel(150000)
 PyPlot.subplot(3,2,6)
 PyPlot.imshow(S_configs[200000,:,:], cmap="hot", interpolation="nearest")
 PyPlot.xlabel(200000)
-PyPlot.savefig("hw_week_3/fig/config_conv.eps", format="eps", dpi=1000)
+PyPlot.savefig("hw_week_3/fig/ising_config_conv.eps", format="eps", dpi=1000)
 
 ################################################################################
 # Exercise: Computing the density of states for the Potts model in 2D with q = 10
 ################################################################################
-
-
-# generate start condiguration for stat S
-dims = 100
-q = 10
-S_start = zeros(dims,dims)
-map!(x -> x = rand(1:q), S_start, S_start)
-
-PyPlot.figure()
-PyPlot.imshow(S_start,cmap="hot", interpolation="nearest")
-PyPlot.colorbar()
 
 # the energy function for the ising model
 function H_potts(S, J)
@@ -294,19 +284,17 @@ function δ(s, s_star)
 end
 
 # Wang-Landau algorithm with one fixed f value
-function wang_landau_one_iteration(S_start, iter, J, q, f)
+function wang_landau_one_iteration(S_start, iter, J, q, f, g_tilde, E)
 
     nbr_stats = length(S_start) # set up
     S_configs = zeros(iter, size(S_start,1),size(S_start,2))
     a_vec = zeros(iter)
-    g_tilde = zeros(2,iter) #[g_tilde, E]
-    g_tilde[1,:] = ones(iter)
+    E_vec = zeros(iter)
 
     S_configs[1,:,:] = S_start # first iteration
-    H_old = H_potts(S_configs[1,:,:], J)
 
-    g_tilde[1,1] = 1
-    g_tilde[2,1] = H_old
+    a_vec[1] = 1
+    E_vec[1] = H_potts(S_start,J)
 
     α_log = log(1)
 
@@ -320,74 +308,80 @@ function wang_landau_one_iteration(S_start, iter, J, q, f)
         H_new = H_potts(S_update,J) # compute energy for prop config
 
         # see if we have curent energy
-        g_tilde_prop_idx = findlast(x -> x == H_new, g_tilde[2,:])
+        g_tilde_prop_idx = findlast(x -> x == H_new, E)
 
         if typeof(g_tilde_prop_idx) == Nothing
             g_tilde_prop = 1
         else
-            g_tilde_prop = g_tilde[1,g_tilde_prop_idx[1]]
+            g_tilde_prop = g_tilde[g_tilde_prop_idx[1]]
         end
 
-        g_tilde_old = g_tilde[1,i-1]
+        g_tilde_old_idx = findlast(x -> x == E_vec[i-1], E)
+        g_tilde_old = g_tilde[g_tilde_old_idx[1]]
 
         α_log = log(g_tilde_old)-log(g_tilde_prop)
 
         if log(rand()) < min(0, α_log) # accapt new config
             S_configs[i,:,:] = S_update
-            g_tilde[2,i] = H_new
+            E_vec[i] = H_new
             a_vec[i] = 1
         else
             S_configs[i,:,:] = S_configs[i-1,:,:] # store old config
-            g_tilde[2,i] = g_tilde[2,i-1]
+            E_vec[i] = E_vec[i-1]
         end
 
         # update energy function for current system
-        g_update_idx = findlast(x -> x == g_tilde[2,i], g_tilde[2,1:i-1])
+        g_update_idx = findlast(x -> x == E_vec[i], E)
 
         if typeof(g_update_idx) == Nothing
             g_update = 1
+            append!(E,[E_vec[i]])
+            append!(g_tilde,[g_update])
         else
-            g_update = g_tilde[1,g_update_idx[1]]
+            g_update = g_tilde[g_update_idx[1]]
+            g_tilde[g_update_idx[1]] = f*g_update
         end
-
-        g_tilde[1,i] = f*g_update
 
     end
 
-    return S_configs, g_tilde, a_vec
+    return S_configs, a_vec, E_vec
 
 end
 
 
 # Wang-Landau algorithm where the f value is decreased
-function wang_landau(nbr_reps,iter, J, q, f, dims = 100)
+function wang_landau(nbr_reps,iter, J, q, f, S_start, g_tilde, E)
 
     println("Starting Wang-Landau.")
 
     # full Wang-Landau algorithm
-    S_start = zeros(dims,dims)
-    g_save = zeros(nbr_reps,2,iter)
     f_save = zeros(nbr_reps)
     S_configs_last = zeros(iter, size(S_start,1),size(S_start,2))
 
     for i = 1:nbr_reps
 
-        # run algorithm
-        S_start = map!(x -> x = rand(1:q), S_start, S_start)
+        S_configs, a_vec = wang_landau_one_iteration(S_start, iter, J, q, f, g_tilde, E)
 
-        S_configs, g_tilde, a_vec = wang_landau_one_iteration(S_start, iter, J, q, f)
-
-        g_save[i,:,:] = g_tilde
         f_save[i] = f
 
         # print info
         @printf "-----------------------------------------------------------------\n"
         @printf "Iter: %f\n" i
-        @printf "f: %f\n" f
-        @printf "Acc. rate: %f %%\n" sum(a_vec)/iter*100
+        @printf "f: %f.0\n" f
+        @printf "Acc. rate: %f.2 %%\n" sum(a_vec)/iter*100
+
+        #=
+        PyPlot.figure()
+        PyPlot.plot(E, g_tilde, "*-")
+        PyPlot.xlabel("Energy")
+        PyPlot.ylabel(L"\tilde{g}")
+        PyPlot.savefig("hw_week_3/fig/potts_g_tilde_iteration_"*string(i)*".eps", format="eps", dpi=1000)
+        =#
 
         # update t
         f = sqrt(f)
+
+        S_start = S_configs[end,:,:]
 
         if i == nbr_reps
             S_configs_last = S_configs
@@ -395,24 +389,77 @@ function wang_landau(nbr_reps,iter, J, q, f, dims = 100)
 
     end
 
-    return g_save, f_save, S_configs_last
+    return f_save, S_configs_last
 
 end
 
 
 # run Wang-Landau algorithm
 
-J = 1 # interaction strength (we have the same interaction strength for all states)
-iter = 10000 # nbr of MC iterations
+# algorithm settings
+J = 0.5 # interaction strength (we have the same interaction strength for all states)
+iter = 1000 # nbr of MC iterations
 f = exp(1)
 nbr_reps = 25 # such that exp(1)^((1/2)^25) \approx exp(10^(-8))
 q = 10 # spins
 
-g_save, f_save, S_configs_last = @time wang_landau(nbr_reps, iter, J, q, f)
+
+# generate start condiguration for stat S
+dims = 100
+S_start = zeros(dims,dims)
+map!(x -> x = rand(1:q), S_start, S_start)
+
+PyPlot.figure()
+PyPlot.imshow(S_start,cmap="hot", interpolation="nearest")
+PyPlot.colorbar()
+PyPlot.savefig("hw_week_3/fig/potts_start_config.eps", format="eps", dpi=1000)
+
+
+# init g_tilde and E vectors
+g_tilde = [1.]
+E = [H_potts(S_start,J)]
+
+f_save, S_configs_last = @time wang_landau(nbr_reps,iter, J, q, f, S_start, g_tilde, E)
+
+
+g_tilde
+
+E
+
+
+S_configs, a_vec, E_vec = wang_landau_one_iteration(S_start, iter, J, q, f, g_tilde, E)
+
+
+a_vec
+
+
+
+E_vec
+
+
+nbr_in_bins, bins, plotobj = PyPlot.plt[:hist](E_vec,100)
+
+minimum(nbr_in_bins)
+maximum(nbr_in_bins)
+
+
+
+PyPlot.figure()
+PyPlot.plot(E, g_tilde, "*-")
+PyPlot.xlabel("Energy")
+PyPlot.ylabel(L"\tilde{g}")
+PyPlot.savefig("hw_week_3/fig/potts_g_tilde_final.eps", format="eps", dpi=1000)
+
+
 
 
 PyPlot.figure()
 PyPlot.plot(f_save)
+PyPlot.xlabel("Iteration")
+PyPlot.ylabel(L"f")
+PyPlot.savefig("hw_week_3/fig/potts_f_vs_iter.eps", format="eps", dpi=1000)
+
+
 PyPlot.title("Scaling")
 
 
